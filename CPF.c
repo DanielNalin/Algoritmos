da0691a9abf84db11a3a4a1005badecad58a7120
DanielNalin <48016234+DanielNalin@users.noreply.github.com>
2021-02-10 23:26:18 -0300

    Update CPF.c

--- CPF.c
+++ CPF.c
@@ -1,66 +1,39 @@
-#include <stdio.h>
-
-int main()
-{
-    char cpf[11];
-    for (int i = 1; i <= 11; i++){
-        scanf("%c", (cpf + i));
+#include <stdio.h>  
+  
+int main()  
+{  
+char cpf[12];  
+int numcpf[12];  
+int i,soma,j,res1,res2,k,x;
+    soma = 0;
+    scanf(" %s",cpf);  
+    for(i=0;i<11;i++){  
+        numcpf[i]=cpf[i]-48;  
+    }  
+    for(i=0;i<9;i++){  
+        soma+=numcpf[i]*(10-i);  
+    }  
+    res1=soma%11;  
+    if( (res1==0) || (res1==1) ){  
+        j=0;  
+    }else{  
+        j = 11-res1;  
+    }  
+    soma=0;  
+    for(i=0;i<10;i++){  
+        soma+=numcpf[i]*(11-i);  
+    }  
+    x=(soma/11)*11;  
+    res2=soma-x;  
+    if( (res2==0) || (res2==1) )  {  
+        k=0;  
+    }else{  
+        k=11-res2;  
     }
-
-    return 0;
-}
-int numsiguais(int a[11]){
-    int cont = 1;
-    for(int i = 1; i <=11; i++){
-        if(a[i] == a[i+1]){
-            cont++;
-        }
-    }
-    return cont >=11;
-}
-int verificar(char cpf[11]){
-    int j;
-    int k;
-    int nums[11];
-    int x;
-    int resto;
-    for (int i = 1; i <=11; i++){
-        if(cpf[i] < '0'|| cpf[i] > '9'){
-            printf("Numero invalido");
-        return 0;
-        }
-    nums[i] = cpf[i] - '0';
-    }
-    if(numsiguais(nums)){
-        printf("Nao eh valido");
-        return 0;
-    }
-    for(int i = 1; i<=9; i++){
-        x = nums[i] * (10 - i) + x;
-    }
-        resto = x % 11;
-    if (resto == 0 || resto == 1){
-        j = 0;
-    }else{
-        j = 11 - resto;
-    }
-    if(j!= nums[9]){
-        printf("Nao eh valido");
-        return 0;
-    }
-    x = 0;
-    for(int i = 1; i<=10; i++){
-        x = nums[i] * (11 - i) + x;
-    }
-    resto = x % 11;
-    if (resto == 0 || resto == 1){
-        k = 0;
-    }else{
-        k = 11 - resto;
-    }
-    if(k!= nums[10]){
-        printf("Nao eh valido");
-        return 0;
-    }
-    printf("Eh valido");
+    if((j==numcpf[9]) && (k==numcpf[10])){  
+        printf("Eh valido");  
+    }else{  
+        printf("Nao eh valido");  
+    }  
+return 0;  
 }
