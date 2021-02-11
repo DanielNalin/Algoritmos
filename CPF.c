8faa578b8514bdce7377c336756e4b57a1dd5303
DanielNalin <48016234+DanielNalin@users.noreply.github.com>
2021-02-10 22:07:34 -0300

    Update CPF.c

--- CPF.c
+++ CPF.c
@@ -27,36 +27,40 @@ int verificar(char cpf[11]){
     for (int i = 1; i <=11; i++){
         if(cpf[i] < '0'|| cpf[i] > '9'){
             printf("Numero invalido");
+        return 0;
         }
     nums[i] = cpf[i] - '0';
     }
     if(numsiguais(nums)){
         printf("Nao eh valido");
+        return 0;
     }
     for(int i = 1; i<=9; i++){
         x = nums[i] * (10 - i) + x;
     }
         resto = x % 11;
-    if (j = resto == 0 || resto == 1){
+    if (resto == 0 || resto == 1){
         j = 0;
     }else{
         j = 11 - resto;
     }
     if(j!= nums[9]){
         printf("Nao eh valido");
+        return 0;
     }
     x = 0;
     for(int i = 1; i<=10; i++){
         x = nums[i] * (11 - i) + x;
     }
     resto = x % 11;
-    if (k = resto == 0 || resto == 1){
+    if (resto == 0 || resto == 1){
         k = 0;
     }else{
         k = 11 - resto;
     }
     if(k!= nums[10]){
         printf("Nao eh valido");
+        return 0;
     }
     printf("Eh valido");
 }
