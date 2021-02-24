8fcbb8a1eee76be2546cb58f397ebc5861de3cf6
DanielNalin <48016234+DanielNalin@users.noreply.github.com>
2021-02-24 12:31:14 -0300

    Create iniciais.c

---
+++ iniciais.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+char name[100];
+int j;
+char initials[20];
+int previousSpace;
+    scanf("%s", name);
+    previousSpace = 1;
+    for (int i = 0; i < 100; i++){
+    if (name[i] == '\0'){
+        break;
+    }
+    if (name[i] == ' '){
+        previousSpace = 1;
+    }
+    else
+    {
+        if (previousSpace == 1){
+            initials[j] = name[i];
+            j++;
+            previousSpace = 0;
+        }
+    }
+}
+    initials[j] = '\0';
+    printf("%s", initials);
+}
