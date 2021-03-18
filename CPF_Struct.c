a5a40c21367edc731d687fffd45e3d4ad6d81e9c
DanielNalin <48016234+DanielNalin@users.noreply.github.com>
2021-03-18 00:24:59 -0300

    Create CPF_Struct.c

---
+++ CPF_Struct.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <string.h>
+#define quantidade 10
+typedef struct pessoa{
+    char nome[20];
+    int idade;
+    char cpf[11];
+} pessoal
+int main()
+{
+    char cpf[11];
+
+    scanf("%s", cpf);
+
+    validarcpf(cpf);
+    return 0;
+}
+int validarcpf(char *cpf)
+{
+  int numcpf[12];
+  int i, soma = 0, j, res1, res2, k, x;
+
+  if ((strcmp(cpf, "00000000000") == 0) || (strcmp(cpf, "11111111111") == 0) || (strcmp(cpf, "22222222222") == 0) ||
+      (strcmp(cpf, "33333333333") == 0) || (strcmp(cpf, "44444444444") == 0) || (strcmp(cpf, "55555555555") == 0) ||
+      (strcmp(cpf, "66666666666") == 0) || (strcmp(cpf, "77777777777") == 0) || (strcmp(cpf, "88888888888") == 0) ||
+      (strcmp(cpf, "99999999999") == 0))
+  {
+    printf("Nao eh valido");
+    return 0;
+  }
+
+  for (i = 0; i < 11; i++)
+  {
+    int couldNumber = cpf[i] - 48;
+
+    if (couldNumber < 0 && couldNumber > 9)
+    {
+      printf("Numero invalido");
+      return 0;
+    }
+    else
+    {
+      numcpf[i] = couldNumber;
+    }
+  }
+
+  for (i = 0; i < 9; i++)
+  {
+    soma += numcpf[i] * (10 - i);
+  }
+
+  res1 = soma % 11;
+
+  if (res1 < 2)
+  {
+    j = 0;
+  }
+  else
+  {
+    j = 11 - res1;
+  }
+
+  soma = 0;
+
+  for (i = 0; i < 10; i++)
+  {
+    soma += numcpf[i] * (11 - i);
+  }
+
+  x = (soma / 11) * 11;
+  res2 = soma - x;
+
+  if (res2 < 2)
+  {
+    k = 0;
+  }
+  else
+  {
+    k = 11 - res2;
+  }
+
+  if ((j == numcpf[9]) && (k == numcpf[10]))
+  {
+    printf("Eh valido");
+  }
+  else
+  {
+    printf("Nao eh valido");
+  }
+  return 0;
+}
